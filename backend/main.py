14a6707a5bdd4338030821a3301fa21060e0a3c3
cyril begard <cyril.begard@epitech.eu>
2021-01-18 10:26:11 +0100

    database

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,23 @@
 from markupsafe import escape
 from flask import Flask
+import sqlite3
 import Twitter
 import Facebook
 import Service
+
+DATABASE = 'dataBase/data.db'
+
+def get_db():
+    db = getattr(Flask, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(Flask, '_database', None)
+    if db is not None:
+        db.close()
 
 app = Flask("AREA")
 Service.setup(app)
