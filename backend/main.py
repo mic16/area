0133edb61e5384767702dab8bec7abbb25ad5643
cyril begard <cyril.begard@epitech.eu>
2021-01-19 10:43:07 +0100

    wait

--- backend/main.py
+++ backend/main.py
@@ -1,26 +1,63 @@
 from markupsafe import escape
-from flask import Flask
-from AccountAPI import account_api
+from flask import Flask, request
 import sqlite3
 import Twitter
 import Facebook
 import Service
 
-DATABASE = 'dataBase/data.db'
 app = Flask("AREA")
 
-app.register_blueprint(account_api)
 
-def get_db():
-    db = getattr(Flask, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
+database_connection = sqlite3.connect("dataBase/data.db", check_same_thread=False)
+database = database_connection.cursor()
 
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(Flask, '_database', None)
-    if db is not None:
-        db.close()
+def init_database():
+    database.execute("CREATE TABLE IF NOT EXISTS Users (id INTEGER PRIMARY KEY AUTOINCREMENT, Username TEXT, Password TEXT)")
 
+init_database()
 Service.setup(app)
+
+
+token = []
+
+class User:
+    def __init__(self, user_tuple):
+        self.id = user_tuple[0]
+        self.Username = user_tuple[1]
+        self.Password = user_tuple[2]
+
+
+def get_user(name):
+    database.execute("SELECT * FROM Users WHERE Username = ?", (name,))
+    user = database.fetchone()
+    if (user != None):
+        return (User(user))
+    else:
+        return (None)
+
+def create_user():
+    database.execute("INSERT INTO Users(Username, Password) VALUES (?, ?)", ()) #TODO
+
+@app.route("/login", methods=['POST'])
+def login():
+    user = get_user(request.form['username'])
+    if (user == None) :
+        return ({"status": False, "message": "user not exist"})
+    elif (user.Password == request.form['password']):
+        return ({"status": True, "message": "user connected"})
+    else :
+        return ({"status": False, "message": "bad password"})
+
+@app.route("/register", methods=['POST'])
+def register():
+    user = get_user(get_user(request.form['username']))
+    if (user != None):
+        return ({"status": False, "message": "user already exist"})
+    else:
+
+
+    pass
+
+@app.route("/logout", methods=['POST'])
+def logout():
+    pass
