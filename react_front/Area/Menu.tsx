d5d6d3438ea907a413aa2bd12f7ca09256657818
Timberloft <yoan.vessiere@epitech.eu>
2021-02-02 15:24:03 +0100

    push stating the menu component for mobile

---
+++ react_front/Area/Menu.tsx
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { ImageBackground, Platform, View } from "react-native";
+import { Footer, FooterTab, Text, Container, Header, Content, Form, Item, Input, Label, Title, Icon, Button, Left, Right } from 'native-base';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
+import LoginComponent from './Login';
+
+
+export default class MenuComponent extends Component {
+
+  state = {
+    loading: true
+  }
+
+  async componentDidMount() {
+      await Font.loadAsync({
+          Roboto: require('native-base/Fonts/Roboto.ttf'),
+          Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+          ...Ionicons.font,
+      });
+      this.setState({ loading: false });
+  }
+
+  render() {
+       if (this.state.loading) {
+         return (
+           <View></View>
+         );
+       }
+       if (Platform.OS == "web")
+        return (
+            <Container style= {{ position: "relative"}}>
+            <Header>
+            
+            </Header>
+            <Content style= {{ position: "relative" }}>
+
+            </Content>
+          </Container>
+        );
+
+        return (
+          <Container>
+          <Header />
+          <Content />
+          <Footer>
+            <FooterTab>
+              <Button vertical>
+                <Icon name="apps" />
+                <Text>Apps</Text>
+              </Button>
+              <Button vertical>
+                <Icon name="camera" />
+                <Text>Camera</Text>
+              </Button>
+              <Button vertical active>
+                <Icon active name="navigate" />
+                <Text>Navigate</Text>
+              </Button>
+              <Button vertical>
+                <Icon name="person" />
+                <Text>Contact</Text>
+              </Button>
+            </FooterTab>
+          </Footer>
+        </Container>
+      );
+   }
+}
+
