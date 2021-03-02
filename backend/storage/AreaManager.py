e130f655399039fc805ab462ae0aea2f2dda05ec
Shiranuit <shiranui007@gmail.com>
2021-03-02 19:03:58 +0100

    test

--- backend/storage/AreaManager.py
+++ backend/storage/AreaManager.py
@@ -33,7 +33,7 @@ class AreaManager(threading.Thread):
             futures = []
             self.ilock.acquire()
             for area in self.areas:
-                futures.append(self.eventLoop.create_task(_exec(area.trigger())))
+                futures.append(self.eventLoop.create_task(_exec(area)))
             self.ilock.release()
             self.eventLoop.run_until_complete(asyncio.gather(*futures))
 
