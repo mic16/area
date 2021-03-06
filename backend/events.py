04a848c978ba6ef259fe45cb164e08d4a7a6a161
Shiranuit <shiranui007@gmail.com>
2021-03-06 17:53:49 +0100

    better events support

--- backend/events.py
+++ backend/events.py
@@ -4,6 +4,20 @@ import json
 
 @app.after_request
 def after_request(response):
+    url = ' %s ' % request.base_url
+    blocklen = len(url) + 2
+    print('='*blocklen)
+    print(url.center(blocklen, '='))
+    print('=%s=' % ('REQUEST'.center(blocklen - 2, '-')))
+    rbody = request.get_data().decode()
+    rtext = json.dumps(json.loads(rbody), indent=4)
+    for i in rtext.split('\n'):
+        print(i)
+    print('=%s=' % ('RESPONSE'.center(blocklen - 2, '-')))
     body = response.get_data().decode()
-    print('%s: %s' % (request.base_url, body))
+    text = json.dumps(json.loads(body), indent=4)
+    for i in text.split('\n'):
+        print(i)
+    print('-'*blocklen)
+    print('='*blocklen)
     return response
