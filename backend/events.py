e91ef8979ddc68bda9cf58bd628028f11526b4bf
Shiranuit <shiranui007@gmail.com>
2021-03-06 18:18:47 +0100

    fix when response or request is missing

--- backend/events.py
+++ backend/events.py
@@ -9,15 +9,15 @@ def after_request(response):
     print('='*blocklen)
     print(url.center(blocklen, '='))
     print('=%s=' % ('REQUEST'.center(blocklen - 2, '-')))
-    rbody = request.get_data().decode()
-    rtext = json.dumps(json.loads(rbody), indent=4)
-    for i in rtext.split('\n'):
-        print(i)
+    if request and request.get_data() and (rbody := request.get_data().decode()):
+        rtext = json.dumps(json.loads(rbody), indent=4)
+        for i in rtext.split('\n'):
+            print(i)
     print('=%s=' % ('RESPONSE'.center(blocklen - 2, '-')))
-    body = response.get_data().decode()
-    text = json.dumps(json.loads(body), indent=4)
-    for i in text.split('\n'):
-        print(i)
+    if response and response.get_data() and (body := response.get_data().decode()):
+        text = json.dumps(json.loads(body), indent=4)
+        for i in text.split('\n'):
+            print(i)
     print('-'*blocklen)
     print('='*blocklen)
     return response
